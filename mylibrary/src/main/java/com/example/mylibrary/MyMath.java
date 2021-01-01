277d6dc2f8fbe90a4e671ed8cd2b67417827b812
narekcyber777 <53946598+narekcyber777@users.noreply.github.com>
2021-01-01 15:12:48 +0400

    Activate Library

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyMath.java
@@ -0,0 +1,20 @@
+package com.example.mylibrary;
+
+public class MyMath {
+
+    public  static  int sum(int ... x){
+        int sum=0;
+        for (int i : x) {
+            sum+=i;
+        }
+        return sum;
+    }
+    public  static  int sub(int ... y){
+        int sub=0;
+        for (int i : y) {
+            sub-=i;
+        }
+        return sub;
+    }
+
+}
